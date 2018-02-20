4786bf8c2dafc858de333606f83f5e10efb80da6
Justo Ruiz Ferrer <justo.ruiz@shapelets.io>
2018-02-20 14:52:34 +0100

    move to conan

--- benchmarks/example.cpp
+++ benchmarks/example.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 
 static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+  //for (auto _ : state)
+  //  std::string empty_string;
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
@@ -12,8 +12,8 @@ BENCHMARK(BM_StringCreation);
 // Define another benchmark
 static void BM_StringCopy(benchmark::State& state) {
   std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
+  //for (auto _ : state)
+  //  std::string copy(x);
 }
 BENCHMARK(BM_StringCopy);
 
