f7300831cf50e090bb9df06305ab1b7b0c831fc8
Justo Ruiz Ferrer <justo.ruiz@shapelets.io>
2018-02-20 20:10:11 +0100

    added an alternative implementation for moving sum based on sequences.  seems to perform sightly better on CPU.

--- benchmarks/movsum.cpp
+++ benchmarks/movsum.cpp
@@ -3,17 +3,52 @@
 #include <vector>
 #include <arrayfire.h>
 
-static void MovingSumConvolve(benchmark::State& state) {
-  af::setBackend(af::Backend::AF_BACKEND_OPENCL);
-  af::array ts = af::randu(state.range(0));
+template<af::Backend BE> void MovingSumConvolve(benchmark::State& state) {
+  af::setBackend(BE);
+  auto tsLen = state.range(0);
   auto m = state.range(1);
+
+  af::array ts = af::randu(tsLen);
   af::array filter = af::constant(1, m);
+
   while (state.KeepRunning()) {
-    af::array cumSum = af::convolve(ts, filter, AF_CONV_EXPAND);
+    af::array movSum = af::convolve(ts, filter, AF_CONV_EXPAND);
+    benchmark::DoNotOptimize(movSum(af::seq(m-1, tsLen-1)));
   }
   af::sync();
 }
 
+template<af::Backend BE> void MovingSumScan(benchmark::State& state) {
+  af::setBackend(BE);
+  auto tsLen = state.range(0);
+  auto m = state.range(1);
+  af::array ts = af::randu(tsLen);
+  while (state.KeepRunning()) {
+    af::array cumsum = af::accum(ts);
+    af::array exCumsum = af::scan(ts, 0, AF_BINARY_ADD, false);
+    benchmark::DoNotOptimize(
+      cumsum(af::seq(m - 1, tsLen - 1)) - exCumsum(af::seq(0, tsLen - m))
+    );
+  }
+  af::sync();
+}
+
+
 // Register the function as a benchmark
-BENCHMARK(MovingSumConvolve)->Ranges({{16<<10, 132<<10}, {512, 512}});
+BENCHMARK_TEMPLATE(MovingSumConvolve, af::Backend::AF_BACKEND_OPENCL)
+  ->RangeMultiplier(8)
+  ->Ranges({{1<<10, 32<<10}, {64, 512}});
+
+BENCHMARK_TEMPLATE(MovingSumConvolve, af::Backend::AF_BACKEND_CPU)
+  ->RangeMultiplier(8)
+  ->Ranges({{1<<10, 32<<10}, {64, 512}});
+
+BENCHMARK_TEMPLATE(MovingSumScan, af::Backend::AF_BACKEND_OPENCL)
+  ->RangeMultiplier(8)
+  ->Ranges({{1<<10, 32<<10}, {64, 512}});
+
+BENCHMARK_TEMPLATE(MovingSumScan, af::Backend::AF_BACKEND_CPU)
+  ->RangeMultiplier(8)
+  ->Ranges({{1<<10, 32<<10}, {64, 512}});
+
 BENCHMARK_MAIN();
