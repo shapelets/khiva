33c0ca13bbdcafc472c0501684e77294fd98ad59
Justo Ruiz Ferrer <justo.ruiz@shapelets.io>
2018-02-21 16:50:59 +0100

    work to do

--- include/tsa.h
+++ include/tsa.h
@@ -9,6 +9,42 @@
 
 using namespace af;
 
+
 namespace tsa {
     const char* version();
+
+    namespace simplification {
+        //array rdp(array source, int maxPoints);
+        //array visvalingam(array source, int maxPoints);         
+    }
+
+    namespace regularization {
+        // array xyz(array source);
+    }
+
+    namespace normalization {
+        // znorm
+        // max min
+        // decimal scaling
+        // adaptive normalization
+    }
+
+    namespace features {
+        // tsfresh
+        // PIP Perceptualy important points
+    }
+
+    namespace dimensionality {
+        // PAA/PLA
+        // SAX
+        // Principal Components A.
+        // Random Projections
+    }
+
+    namespace distances {
+        // habituales + la ncc
+
+        // una funcion que dada un tipo de destiancia retorna 
+        // una matriz triangular de distancias entre timeseries 
+    }
 }
