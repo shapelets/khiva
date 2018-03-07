87c76f9e93fb34bc81817781aab57375314fb666
David Cuesta <david.cuesta@shapelets.io>
2018-03-07 16:42:49 +0100

    JNI interface added for using findBestNMotifs function from Java

--- include/tsa.h
+++ include/tsa.h
@@ -490,6 +490,7 @@ namespace tsa {
         #ifdef __cplusplus
         extern "C"{
         #endif
+        
         /**
          * @brief Primitive for calling findBestNMotifs function in order to extract the best N motifs from a previously calculated matrix profile
          * 
@@ -499,7 +500,7 @@ namespace tsa {
          * @param n Number of motifs to extract
          * @param motif_distances The distance of the best N motifs
          * @param motif_indices The indices of the best N motifs
-         * @param subsequence_indexes The indices of the query sequences that produced the minimum reported in the motifs
+         * @param subsequence_indices The indices of the query sequences that produced the minimum reported in the motifs
          */
         void find_best_n_motifs(double* profile, unsigned int* index, long* length_profile, long* n, 
                                 double* motif_distances, int* motif_indices, int* subsequence_indices);
@@ -529,6 +530,21 @@ namespace tsa {
          * @param i Pointer to an initialized  array of doubles for storing the index profile.
          */
         void stomp_self_join(double* ta, int* lta, long*m, double* p, unsigned int* i);
+
+        /**
+         * @brief JNI interface for calling findBestNMotifs function in order to extract the best N motifs from a previously calculated matrix profile
+         * 
+         * @param profile The matrix profile containing the minimum distance of each subsequence
+         * @param index The matrix profile index containing where each minimum occurs
+         * @param lengthProfile Length of the matrix profile
+         * @param n Number of motifs to extract
+         * @param jMotifDistances The distance of the best N motifs
+         * @param jMotifIndices The indices of the best N motifs
+         * @param jSubsequenceIndices The indices of the query sequences that produced the minimum reported in the motifs
+         */
+        JNIEXPORT void JNICALL Java_tsa_TSA_findBestNMotifs(JNIEnv *env, jobject thisObj, jdoubleArray profile, jintArray index, 
+                                                            jlong lengthProfile, jlong n, jdoubleArray jMotifDistances, jintArray jMotifIndices,
+                                                            jintArray jSubsequenceIndices);
 
         /**
          * @brief JNI interface for calling the STOMP algorithm to calculate the matrix profile between 'ta' and 'tb' using a subsequence length
