ae88fa37635c177f39f2fde8b2d678793aad11c5
Oscar Torreno <oscar.torreno@shapelets.io>
2018-04-30 09:03:01 +0000

    Merged in fix/min_epsilon_normalization (pull request #124)
    Fixing a problem that occurred when the standard deviation of the query subsequence was zero.
    
    Approved-by: David Cuesta <david.cuesta@shapelets.io>

--- include/tsa/normalization.h
+++ include/tsa/normalization.h
@@ -15,7 +15,7 @@ namespace normalization {
  *
  * @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and dimension
  * one indicates the number of time series.
- * @param epsilon Minimum standard deviation to consider.  It acts a a gatekeeper for those time series that may be
+ * @param epsilon Minimum standard deviation to consider.  It acts as a gatekeeper for those time series that may be
  * constant or near constant.
  *
  * @return af::array With the same dimensions as tss where the time series have been adjusted for zero mean and one as
@@ -28,13 +28,13 @@ af::array znorm(af::array tss, double epsilon = 0.00000001);
  *
  * @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and dimension
  * one indicates the number of time series.
- * @param epsilon Minimum standard deviation to consider. It acts a a gatekeeper for those time series that may be
+ * @param epsilon Minimum standard deviation to consider. It acts as a gatekeeper for those time series that may be
  * constant or near constant.
  */
 void znormInPlace(af::array &tss, double epsilon = 0.00000001);
 
 /**
- * @brief Normalizes the given time series according to its minimum and maximun value and adjusts each value within the
+ * @brief Normalizes the given time series according to its minimum and maximum value and adjusts each value within the
  * range [low, high].
  *
  * @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and dimension
@@ -45,7 +45,7 @@ void znormInPlace(af::array &tss, double epsilon = 0.00000001);
  * between min and max values in the tss.
  *
  * @return af::array An array with the same dimensions as tss, whose values (time series in dimension 0) have been
- * normalized by maximun and minimun values, and scaled as per high and low parameters.
+ * normalized by maximum and minimum values, and scaled as per high and low parameters.
  */
 af::array maxMinNorm(af::array tss, double high = 1.0, double low = 0.0, double epsilon = 0.00000001);
 
@@ -62,7 +62,7 @@ af::array maxMinNorm(af::array tss, double high = 1.0, double low = 0.0, double 
 void maxMinNormInPlace(af::array &tss, double high = 1.0, double low = 0.0, double epsilon = 0.00000001);
 
 /**
- * @brief Normalizes the given time series according to its maximun value and adjusts each value within the range
+ * @brief Normalizes the given time series according to its maximum value and adjusts each value within the range
  * (-1, 1).
  *
  * @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and dimension
