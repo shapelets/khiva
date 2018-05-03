e431fc126621ca65c73962cba8fff5e5395e9e17
Antonio Vilches <antonio.vilches@shapelets.io>
2018-05-03 18:41:06 +0200

    Adding hamming distance

--- include/tsa/distances.h
+++ include/tsa/distances.h
@@ -57,6 +57,18 @@ af::array euclidean(af::array tss);
 af::array hamming(af::array tss);
 
 /**
+ * @brief Calculates manhattan distances between time series.
+ *
+ * @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and
+ * dimension one indicates the number of time series.
+ *
+ * @return af::array An upper triangular matrix where each position corresponds to the distance between two
+ * time series. Diagonal elements will be zero. For example: Position row 0 column 1 records the distance
+ * between time series 0 and time series 1.
+ */
+af::array manhattan(af::array tss);
+
+/**
  * @brief Calculates non squared version of the euclidean distance.
  *
  * @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and
