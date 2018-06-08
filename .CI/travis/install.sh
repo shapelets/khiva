e125f2a064e63961942d8d5e3d9e82603ae4c56b
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-08 13:15:58 +0200

    testing 7

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# Sudo
-sudo apt-get install libboost-all-dev
+# Installing dependencies
+sudo apt-get update
+sudo apt-get install -y python3 pip3 cmake
+
+# Installing conan
+pip3 install conan
+
+# Installing library dependencies with conan
+conan install . --build missing -s compiler.libcxx=libstdc++11
+
+# Preparing the build
+mkdir build
+cd build
+cmake ..
+make all
