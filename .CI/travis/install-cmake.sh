d54ab28a1c77814cc3119d2cc545c1628360965b
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-11 21:54:58 +0200

    test1

--- .CI/travis/install-cmake.sh
+++ .CI/travis/install-cmake.sh
@@ -10,6 +10,7 @@ sudo apt-get remove cmake
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip libboost-all-dev
 sudo pip install --upgrade cmake
+echo $PATH
 
 # sudo apt-get remove cmake
 # wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
