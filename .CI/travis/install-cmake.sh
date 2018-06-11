bb556e791f13c1a727909bbd043525a27331b247
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-11 14:55:22 +0200

    test2

--- .CI/travis/install-cmake.sh
+++ .CI/travis/install-cmake.sh
@@ -5,11 +5,10 @@
 ###################
 
 wget https://cmake.org/files/v3.11/cmake-3.11.3.tar.gz
-tar -xzvf cmake-3.11.3.tar.gz
+tar -xzf cmake-3.11.3.tar.gz
 cd cmake-3.11.3
 ./bootstrap > /dev/null 2>&1
 sudo make install
-cd ..
-
 # To update to the new version
 sudo update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
+cd ..
