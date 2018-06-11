014933d4c1470b0acf2649d81a7c31b86dfb2cdf
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-11 17:47:41 +0200

    test1

--- .CI/travis/install-cmake.sh
+++ .CI/travis/install-cmake.sh
@@ -9,4 +9,4 @@ wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
 chmod +x cmake-3.11.3-Linux-x86_64.sh
 sudo ./cmake-3.11.3-Linux-x86_64.sh --skip-license --prefix=/usr/local
 sudo ln -s /usr/local/bin/cmake /usr/bin/cmake
-cd ..
+rm cmake-3.11.3-Linux-x86_64.sh
