72dcaaee401e2ed61d0a10e0db529cd08df82157
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-12 22:33:58 +0200

    Fixing codecov

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd build
+lcov --zerocounters  --directory .
+make test
 lcov --directory test --base-directory .. --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '/opt*' '*/cl.hpp' -o coverage.info
 bash <(curl -s https://codecov.io/bash)
