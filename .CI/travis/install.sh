7d00e5a39c5af52c68b9d2c8b171701821a2453e
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-12 13:17:13 +0200

    test1

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -9,6 +9,6 @@ sudo pip3 install conan sphinx sphinx_rtd_theme breathe
 sudo pip install --user cpp-coveralls
 
 # Installing library dependencies with conan
-conan remote add conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
-conan remote add boost_math https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
+conan remote add -f boost_math https://api.bintray.com/conan/bincrafters/public-conan
 conan install . --build missing 2>&1 > /dev/null
