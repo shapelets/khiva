50743174fd538efc283195238709c7c9e399c335
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-13 00:40:01 +0200

    test1

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -5,6 +5,8 @@ lcov --zerocounters --directory .
 lcov --capture --initial --directory . --base-directory ../src --no-external --output-file coverage.info
 make test
 lcov --directory . --base-directory ../src --no-external --capture --output-file coverage.info
-lcov --remove coverage.info '/usr*' '/opt*' '*/cl.hpp' -o coverage.info
-bash <(curl -s https://codecov.io/bash)
+lcov --remove coverage.info '/usr*' '/opt*' '*/cl.hpp' '*/.conan/*' -o coverage.info
+lcov --list coverage.info
+# Uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
 cd ..
