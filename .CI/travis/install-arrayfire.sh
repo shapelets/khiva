c3a8aaeb1c6aa1293dfd6068f4ff112751a3264f
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-12 12:15:33 +0200

    test1

--- .CI/travis/install-arrayfire.sh
+++ .CI/travis/install-arrayfire.sh
@@ -4,13 +4,11 @@
 # Install Arrayfire
 ###################
 
+wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
+mkdir /opt/arrayfire
 
-if [ ! -d "/opt/arrayfire" ]; then
-    wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
-    mkdir /opt/arrayfire
+chmod +x ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
+./ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh --prefix=/opt/arrayfire --skip-license
+rm ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
 
-    chmod +x ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
-    ./ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh --prefix=/opt/arrayfire --skip-license
-    rm ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
-fi
 
