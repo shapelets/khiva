4e99458d545910f50e437d4e2851cd3408a7585d
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-13 01:05:05 +0200

    test3

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd build
-lcov --zerocounters --directory .
-lcov --capture --initial --directory . --base-directory ../src --no-external --output-file coverage.info
+# lcov --zerocounters --directory .
+# lcov --capture --initial --directory . --base-directory ../src --no-external --output-file coverage.info
 make test
 lcov --directory . --base-directory ../src --no-external --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '/opt*' '*/cl.hpp' '*/.conan/*' -o coverage.info
