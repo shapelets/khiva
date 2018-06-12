c018f479637bfc07956e1d2c94b06fc8c6500e9b
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-12 17:12:59 +0200

    Fixing codecov

--- .CI/travis/coveralls.sh
+++ .CI/travis/coveralls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd build
-coveralls --root .. -E ".*external.*" -E ".*CMakeFiles.*" -E ".*test/.*" -E ".*bindings/.*" -E ".*benchmarks/.*" -E ".*cmake/.*" -E "/usr/*.*" -E "/opt/*.*" -E "/home/travis/.conan/*.*" 
+coveralls --root .. -E ".*external.*" -E ".*CMakeFiles.*" -E ".*test/.*" -E ".*bindings/.*" -E ".*benchmarks/.*" -E ".*cmake/.*" -E "/usr/*.*" -E "/opt/*.*" -E "/home/travis/.conan/*.*" --exclude lib --exclude tests --gcov-options '\-lp' 
 cd ..
