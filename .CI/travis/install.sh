f94528faed5c07c872dfbbaf023e5171c3a313b5
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-12 15:48:43 +0200

    test1

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y python3 python3-pip libboost-all-dev doxygen graphviz
 sudo pip3 install conan sphinx sphinx_rtd_theme breathe
 
 # Installing Coveralls
-sudo pip install --user cpp-coveralls
+sudo pip3 install cpp-coveralls
 
 # Installing library dependencies with conan
 conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
