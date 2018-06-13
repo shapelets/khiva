1cae95c7a0a805b9025b92782a58ca88e24951b2
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-13 10:53:27 +0200

    Change in codecov.sh

--- .CI/travis/install-cmake.sh
+++ .CI/travis/install-cmake.sh
@@ -5,8 +5,6 @@
 ###############
 
 # Installing dependencies
-
-sudo apt-get remove cmake
 wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
 chmod +x cmake-3.11.3-Linux-x86_64.sh
 sudo ./cmake-3.11.3-Linux-x86_64.sh --skip-license
