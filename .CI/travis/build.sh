3d08361c21a8a079aef37b5ca2a7300f8862f089
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-14 07:12:21 +0200

    Updating CodeCov

--- .CI/travis/build.sh
+++ .CI/travis/build.sh
@@ -3,6 +3,7 @@
 
 # Build the project
 mkdir -p build && cd build
-../bin/cmake .. -DKHIVA_COVERAGE=ON
+../bin/cmake .. -DKHIVA_ENABLE_COVERAGE=ON
 make all
+make test
 cd ..
--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd build
-lcov --zerocounters --directory .
-make test
-lcov --directory .. --base-directory ../src --no-external --capture --output-file coverage.info
+
+lcov --directory . --no-external --capture --output-file coverage.info
 lcov --remove coverage.info 'test/*' 'include/*' -o coverage.info
 lcov --list coverage.info
 
