30c40f5f7836ea9c89a98eb8282d4e48801fd6d1
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-14 09:19:52 +0200

    updating codecov

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 lcov --directory . --no-external --capture --output-file coverage.info
 lcov --remove coverage.info 'test/*' 'include/*' -o coverage.info
 lcov --list coverage.info
 
 # Uploading report to CodeCov
-bash <(curl -s https://codecov.io/bash) -X gcov
+bash <(curl -s https://codecov.io/bash)
