c88423a0719014292c5431bdfa424aa9cfcd185a
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-14 10:25:21 +0200

    test1

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd build
-lcov --directory src --base-directory ../src/khiva --no-external --capture --output-file coverage.info
+lcov --directory . --base-directory ../src/khiva --no-external --capture --output-file coverage.info
 lcov --remove coverage.info 'Core*' '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include*' -o coverage.info
 lcov --list coverage.info
 
 # Uploading report to CodeCov
-bash <(curl -s https://codecov.io/bash)
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
