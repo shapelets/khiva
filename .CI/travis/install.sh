30c6bb7a8442afe0a81624ecfe282e0628f71d27
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-15 11:28:04 +0200

    Fixing travis

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -6,4 +6,3 @@ pip3 install conan sphinx sphinx_rtd_theme breathe
 # Installing library dependencies with conan
 conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
 conan remote add -f boost_math https://api.bintray.com/conan/bincrafters/public-conan
-conan install . --build missing 
