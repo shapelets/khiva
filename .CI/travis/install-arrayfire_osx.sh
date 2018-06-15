440142bb75b81c64ebea1036ba8ecdc0bc438d22
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-16 01:23:35 +0200

    Fixing Array package for macos

--- .CI/travis/install-arrayfire_osx.sh
+++ .CI/travis/install-arrayfire_osx.sh
@@ -4,9 +4,9 @@
 # Install Arrayfire
 ###################
 
-wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
+wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-v3.6.0_macOS_x86_64.sh
 sudo mkdir /opt/arrayfire
 
-chmod +x ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
-sudo ./ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh --prefix=/opt/arrayfire --skip-license
-rm ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
+chmod +x ArrayFire-v3.6.0_macOS_x86_64.sh
+sudo ./ArrayFire-v3.6.0_macOS_x86_64.sh --prefix=/opt/arrayfire --skip-license
+rm ArrayFire-v3.6.0_macOS_x86_64.sh
