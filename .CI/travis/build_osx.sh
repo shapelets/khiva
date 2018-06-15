3c2e106e3714390660bd768542339e4b9d235de8
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-15 17:56:18 +0200

    Adding travis file and code coverage

---
+++ .CI/travis/build_osx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# Build the project
+mkdir -p build && cd build
+cmake .. -DKHIVA_ENABLE_COVERAGE=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
+make all
+make test
+cd ..
