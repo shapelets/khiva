d25e0e2f0efd22b2bd15232021ca7f3b99848ac9
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-16 01:36:04 +0200

    adding source command to install arrayfire

--- .CI/travis/install-arrayfire_osx.sh
+++ .CI/travis/install-arrayfire_osx.sh
@@ -8,5 +8,5 @@ wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-v3.6.0_macOS_x86_64.sh
 sudo mkdir /opt/arrayfire
 
 chmod +x ArrayFire-v3.6.0_macOS_x86_64.sh
-sudo ./ArrayFire-v3.6.0_macOS_x86_64.sh --prefix=/opt/arrayfire --skip-license
+sudo source ArrayFire-v3.6.0_macOS_x86_64.sh --prefix=/opt/arrayfire --skip-license
 rm ArrayFire-v3.6.0_macOS_x86_64.sh
