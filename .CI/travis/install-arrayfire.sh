a06e9c34e5e4dce4ad48521bef5634e8fb750565
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-15 09:15:26 +0200

    Solving issues in matrix

--- .CI/travis/install-arrayfire.sh
+++ .CI/travis/install-arrayfire.sh
@@ -5,10 +5,10 @@
 ###################
 
 wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
-mkdir /opt/arrayfire
+sudo mkdir /opt/arrayfire
 
 chmod +x ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
-./ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh --prefix=/opt/arrayfire --skip-license
+sudo ./ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh --prefix=/opt/arrayfire --skip-license
 rm ArrayFire-no-gl-v3.6.0_Linux_x86_84.sh
 
 
