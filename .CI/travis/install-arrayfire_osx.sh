91fd5277d67096f0bf06d36dec56092b60044ccf
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-16 01:48:00 +0200

    changing arayfire installation

--- .CI/travis/install-arrayfire_osx.sh
+++ .CI/travis/install-arrayfire_osx.sh
@@ -4,9 +4,9 @@
 # Install Arrayfire
 ###################
 
-wget http://arrayfire.s3.amazonaws.com/3.6.0/ArrayFire-v3.6.0_macOS_x86_64.sh
+wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-no-gl-v3.5.1_OSX.pkg
 sudo mkdir /opt/arrayfire
 
 chmod +x ArrayFire-v3.6.0_macOS_x86_64.sh
-sudo source ArrayFire-v3.6.0_macOS_x86_64.sh --prefix=/opt/arrayfire --skip-license
-rm ArrayFire-v3.6.0_macOS_x86_64.sh
+sudo installer -pkg ArrayFire-no-gl-v3.5.1_OSX.pkg -target /opt/arrayfire
+rm ArrayFire-no-gl-v3.5.1_OSX.pkg
