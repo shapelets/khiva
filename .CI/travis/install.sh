1a3a221a3b4def7de8090e3758b89b484dc6822d
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-15 10:03:55 +0200

    test1

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Installing conan
-sudo pip3 install conan sphinx sphinx_rtd_theme breathe
-
-# Installing Coveralls
-sudo pip3 install cpp-coveralls
+pip3 install conan sphinx sphinx_rtd_theme breathe
 
 # Installing library dependencies with conan
 conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
