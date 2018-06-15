f9b8f65ff8edf3bc7fc77226e291a7e1f2104361
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-15 18:50:16 +0200

    Fixing install in macos

--- .CI/travis/install_osx.sh
+++ .CI/travis/install_osx.sh
@@ -3,3 +3,10 @@
 brew update
 brew upgrade python
 brew install --force doxygen graphviz
+
+# Installing conan
+sudo pip install conan sphinx sphinx_rtd_theme breathe
+
+# Installing library dependencies with conan
+conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
+conan remote add -f boost_math https://api.bintray.com/conan/bincrafters/public-conan
