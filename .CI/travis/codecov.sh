0f62a3696522d02673d262e9871a855e90fbbe9b
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-15 13:39:13 +0200

    Adding travis file and code coverage

---
+++ .CI/travis/codecov.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd build
+lcov --directory . --base-directory ../src/khiva --no-external --capture --output-file coverage.info
+lcov --remove coverage.info '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include*' -o coverage.info
+lcov --list coverage.info
+
+# Uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
---
+++ .CI/travis/install_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install -y lcov python3 python3-pip libboost-all-dev doxygen graphviz
+
+# Installing conan
+pip3 install conan sphinx sphinx_rtd_theme breathe
+
+# Installing library dependencies with conan
+conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
+conan remote add -f boost_math https://api.bintray.com/conan/bincrafters/public-conan
