99fe38a43c52a612ae5f9b8f0e1e9d805f410036
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-18 12:00:33 +0200

    Adding codecov for macos build

--- .CI/travis/build_osx.sh
+++ .CI/travis/build_osx.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-export AF_PATH=/opt/arrayfire
-
 # Build the project
 mkdir -p build && cd build
-cmake .. -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
+cmake .. -DKHIVA_ENABLE_COVERAGE=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
 make all
-AF_PRINT_ERRORS=1 ./bin/dimensionality
 make test
-
 cd ..
