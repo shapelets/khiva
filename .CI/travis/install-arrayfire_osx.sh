c686574638b9c4df49bb76c6abdc568e1b85a587
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-18 18:19:25 +0200

    Setting arrayfire with opencl integration

--- .CI/travis/install-arrayfire_osx.sh
+++ .CI/travis/install-arrayfire_osx.sh
@@ -5,9 +5,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-no-gl-v3.5.1_OSX.pkg
+wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_OSX.pkg
 sudo mkdir /opt/arrayfire
 
 # chmod +x ArrayFire-no-gl-v3.5.1_OSX.pkg
-sudo installer -pkg ArrayFire-no-gl-v3.5.1_OSX.pkg -target /
-rm ArrayFire-no-gl-v3.5.1_OSX.pkg
+sudo installer -pkg ArrayFire-v3.5.1_OSX.pkg -target /
+rm ArrayFire-v3.5.1_OSX.pkg
