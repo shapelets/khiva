d5edb766208b77022350637982f7e0e4f6c39cb1
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-18 18:29:35 +0200

    Cleaning travis file

--- .CI/travis/install-arrayfire_osx.sh
+++ .CI/travis/install-arrayfire_osx.sh
@@ -8,6 +8,5 @@
 wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_OSX.pkg
 sudo mkdir /opt/arrayfire
 
-# chmod +x ArrayFire-no-gl-v3.5.1_OSX.pkg
 sudo installer -pkg ArrayFire-v3.5.1_OSX.pkg -target /
 rm ArrayFire-v3.5.1_OSX.pkg
