1b13303ef274563ae8edcf55e027f6dde2a981b5
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-18 10:05:50 +0200

    Adding version test

--- .CI/travis/build_osx.sh
+++ .CI/travis/build_osx.sh
@@ -5,6 +5,7 @@
 mkdir -p build && cd build
 cmake .. -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
 make all
+./bin/version
 ./bin/dimensionality
 make test
 
