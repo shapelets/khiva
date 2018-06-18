0057b93dc1344946da858abadb60c1bd58d0dc4a
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-18 14:10:40 +0200

    Adding KHIVA_ONLY_CPU_BACKEND option

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd build
-lcov --directory . --base-directory ../src/khiva --no-external --capture --output-file coverage.info
+lcov --directory . --capture --no-external --output-file coverage.info
 lcov --remove coverage.info '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include/*' '*/bindings/*' '*/test/*' -o coverage.info
 lcov --list coverage.info
 
