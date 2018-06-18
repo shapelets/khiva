09e5ff577f8cd61df0690ca916c6433f047a97d8
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-18 12:12:17 +0200

    Excluding dirs from coverage

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd build
-lcov --directory . --base-directory ../src/khiva --no-external --capture --output-file coverage.info
-lcov --remove coverage.info '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include*' -o coverage.info
+lcov --directory . --base-directory ../src --no-external --capture --output-file coverage.info
+lcov --remove coverage.info '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include/*' '*/bindings/*' '*/test/*' -o coverage.info
 lcov --list coverage.info
 
 # Uploading report to CodeCov
