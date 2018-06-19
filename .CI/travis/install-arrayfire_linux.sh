9782748424a2eacbf1a156edf37cc28fede869e0
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-19 16:32:48 +0200

    Caching ArrayFire and Cmake

--- .CI/travis/install-arrayfire_linux.sh
+++ .CI/travis/install-arrayfire_linux.sh
@@ -5,7 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if [ ! -d "${TRAVIS_BUILD_DIR}/arrayfire" ]; then
+#if [ ! -d "${TRAVIS_BUILD_DIR}/arrayfire" ]; then
     mkdir -p arrayfire && cd arrayfire
     wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_Linux_x86_64.sh
     sudo mkdir /opt/arrayfire -p
@@ -13,4 +13,4 @@ if [ ! -d "${TRAVIS_BUILD_DIR}/arrayfire" ]; then
     chmod +x ArrayFire-v3.5.1_Linux_x86_64.sh
     sudo ./ArrayFire-v3.5.1_Linux_x86_64.sh --prefix=/opt/arrayfire --skip-license
     cd ..
-fi
+#fi
