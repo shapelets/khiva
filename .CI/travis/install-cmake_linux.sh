147b60e36f6134df25a911db22ad7f3d69bd879b
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-19 14:09:21 +0200

    Caching ArrayFire and Cmake

--- .CI/travis/install-cmake_linux.sh
+++ .CI/travis/install-cmake_linux.sh
@@ -6,9 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [ ! -d "${TRAVIS_BUILD_DIR/cmake}" ]; then
-    mkdir -p cmake
-
-    cd cmake
+    mkdir -p cmake && cd cmake
     wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
     chmod +x cmake-3.11.3-Linux-x86_64.sh
     sudo ./cmake-3.11.3-Linux-x86_64.sh --skip-license
