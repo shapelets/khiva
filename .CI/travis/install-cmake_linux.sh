7ee0f39e4140d47b014794367542d0a5d2ed0e24
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-19 16:59:55 +0200

    Caching ArrayFire and Cmake

--- .CI/travis/install-cmake_linux.sh
+++ .CI/travis/install-cmake_linux.sh
@@ -10,6 +10,5 @@
     wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
     chmod +x cmake-3.11.3-Linux-x86_64.sh
     sudo ./cmake-3.11.3-Linux-x86_64.sh --skip-license
-    rm cmake-3.11.3-Linux-x86_64.sh
     cd ..
 #fi
