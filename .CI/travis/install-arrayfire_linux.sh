5eb3229f42175b2192c903964f5d2ee5ee726e97
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-19 12:06:14 +0200

    Caching ArrayFire and Cmake

--- .CI/travis/install-arrayfire_linux.sh
+++ .CI/travis/install-arrayfire_linux.sh
@@ -5,15 +5,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+if [ ! -d "/opt/arrayfire" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_Linux_x86_64.sh
+    sudo mkdir /opt -p
 
-#if [ ! -d "/opt/arrayfire" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist.
-  wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_Linux_x86_64.sh
-  sudo mkdir /opt -p
+    chmod +x ArrayFire-v3.5.1_Linux_x86_64.sh
+    sudo ./ArrayFire-v3.5.1_Linux_x86_64.sh --prefix=/opt --skip-license
 
-  chmod +x ArrayFire-v3.5.1_Linux_x86_64.sh
-  sudo ./ArrayFire-v3.5.1_Linux_x86_64.sh --prefix=/opt --skip-license
-
-  rm ArrayFire-v3.5.1_Linux_x86_64.sh
-#fi
-
+    rm ArrayFire-v3.5.1_Linux_x86_64.sh
+fi
--- .CI/travis/install-cmake_linux.sh
+++ .CI/travis/install-cmake_linux.sh
@@ -5,13 +5,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#if [ ! -d "${TRAVIS_BUILD_DIR/cmake}" ]; then
-mkdir -p cmake
+if [ ! -d "${TRAVIS_BUILD_DIR/cmake}" ]; then
+    mkdir -p cmake
 
-cd cmake
-wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
-chmod +x cmake-3.11.3-Linux-x86_64.sh
-sudo ./cmake-3.11.3-Linux-x86_64.sh --skip-license
-rm cmake-3.11.3-Linux-x86_64.sh
-cd ..
-#fi
+    cd cmake
+    wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
+    chmod +x cmake-3.11.3-Linux-x86_64.sh
+    sudo ./cmake-3.11.3-Linux-x86_64.sh --skip-license
+    rm cmake-3.11.3-Linux-x86_64.sh
+    cd ..
+fi
