f8ea61d72d17e7911c86a17e5207177cbff49fe1
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-19 18:53:15 +0200

    Caching ArrayFire and Cmake

--- .CI/travis/install_osx.sh
+++ .CI/travis/install_osx.sh
@@ -5,9 +5,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-brew update
-brew upgrade python
-brew install --force doxygen graphviz lcov
+#brew update
+#brew upgrade python
+brew install --force python doxygen graphviz lcov
 
 # Installing conan
 sudo pip3 install --upgrade pip
