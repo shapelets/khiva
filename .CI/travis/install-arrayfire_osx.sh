0879cbc5fb38ce3bbc7b9e9c2d7b4a8ba5a0bf70
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-19 13:05:18 +0200

    Caching ArrayFire and Cmake

--- .CI/travis/install-arrayfire_osx.sh
+++ .CI/travis/install-arrayfire_osx.sh
@@ -6,6 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [ ! -d "/opt/arrayfire" ]; then
+
     wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-no-gl-v3.5.1_OSX.pkg
     sudo mkdir /opt/arrayfire
 
