03ffaf7f91dc14bcd2ce5ec13a240a8ce7ad4f78
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-28 20:46:36 +0200

    Cd was necessary

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -4,7 +4,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+cd ..
 lcov --directory src --capture --no-external --output-file coverage.info
 lcov --remove coverage.info '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include/*' '*/bindings/*' '*/test/*' -o coverage.info
 lcov --list coverage.info
