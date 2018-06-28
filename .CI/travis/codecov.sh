7b538c52b57a6a24f0eca0d23015ab4339d9b650
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-28 20:36:53 +0200

    Fix in the codecov path

--- .CI/travis/codecov.sh
+++ .CI/travis/codecov.sh
@@ -5,7 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 cd ..
-lcov --directory . --capture --no-external --output-file coverage.info
+lcov --directory src --capture --no-external --output-file coverage.info
 lcov --remove coverage.info '*/.conan/*' '*/usr*' '*/cl.hpp' '*/include/*' '*/bindings/*' '*/test/*' -o coverage.info
 lcov --list coverage.info
 
