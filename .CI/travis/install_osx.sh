9e0a2ce4d236e5e120b3bb663d2c5fef2a83ad41
Oscar Torreno <oscar.torreno@shapelets.io>
2018-07-02 08:26:08 +0200

    Remove directory change

--- .CI/travis/install_osx.sh
+++ .CI/travis/install_osx.sh
@@ -6,10 +6,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Forcing Python 3.6.5_1. Pip packages using cython do not compile with 3.7.0.
-cd $( brew --prefix )
-git checkout f2a764e ./Homebrew/Library/Taps/homebrew/homebrew-core/Formula/python.rb
+BREW_PREFIX=$( brew --prefix )
+git checkout f2a764e ${BREW_PREFIX}/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/python.rb
 brew upgrade python
-git checkout -- ./Homebrew/Library/Taps/homebrew/homebrew-core/Formula/python.rb
+git checkout -- ${BREW_PREFIX}/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/python.rb
 
 brew install --force lcov
 
