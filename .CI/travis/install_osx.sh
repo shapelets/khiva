fbc851ae9ad1dd878157c354b35eae81b431729c
Oscar Torreno <oscar.torreno@shapelets.io>
2018-07-02 08:42:24 +0200

    Enter the right homebrew folder

--- .CI/travis/install_osx.sh
+++ .CI/travis/install_osx.sh
@@ -6,10 +6,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Forcing Python 3.6.5_1. Pip packages using cython do not compile with 3.7.0.
-BREW_PREFIX=$( brew --prefix )
-git checkout f2a764e ${BREW_PREFIX}/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/python.rb
+cd "$(brew --repo homebrew/core)"
+git checkout f2a764e Formula/python.rb
 brew upgrade python
-git checkout -- ${BREW_PREFIX}/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/python.rb
+git checkout -- Formula/python.rb
+cd $OLDPWD
 
 brew install --force lcov
 
