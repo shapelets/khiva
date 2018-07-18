1307fe5d0e3229f9ef175772200455959b9b6988
Oscar Torreno <oscar.torreno@shapelets.io>
2018-07-18 12:52:25 +0200

    Catching findBestN motifs and discords exceptions in the binding side

--- bindings/c/src/matrix.cpp
+++ bindings/c/src/matrix.cpp
@@ -7,6 +7,7 @@
 #include <arrayfire.h>
 #include <khiva/matrix.h>
 #include <khiva_c/matrix.h>
+#include <stdexcept>
 
 void find_best_n_discords(khiva_array *profile, khiva_array *index, long *m, long *n, khiva_array *discord_distances,
                           khiva_array *discord_indices, khiva_array *subsequence_indices, bool *self_join) {
@@ -18,8 +19,18 @@ void find_best_n_discords(khiva_array *profile, khiva_array *index, long *m, lon
     af::array discords;
     af::array discordIndices;
     af::array subsequenceIndices;
-    khiva::matrix::findBestNDiscords(var_profile, var_index, *m, *n, discords, discordIndices, subsequenceIndices,
-                                     self_join);
+
+    try {
+        khiva::matrix::findBestNDiscords(var_profile, var_index, *m, *n, discords, discordIndices, subsequenceIndices,
+                                         self_join);
+    } catch (std::invalid_argument &ia) {
+        std::cerr << ia.what() << std::endl;
+        exit(-1);
+    } catch (std::runtime_error &re) {
+        std::cerr << re.what() << std::endl;
+        exit(-1);
+    }
+
     af_retain_array(discord_distances, discords.get());
     af_retain_array(discord_indices, discordIndices.get());
     af_retain_array(subsequence_indices, subsequenceIndices.get());
@@ -35,7 +46,18 @@ void find_best_n_motifs(khiva_array *profile, khiva_array *index, long *m, long 
     af::array motifs;
     af::array motifIndices;
     af::array subsequenceIndices;
-    khiva::matrix::findBestNMotifs(var_profile, var_index, *m, *n, motifs, motifIndices, subsequenceIndices, self_join);
+
+    try {
+        khiva::matrix::findBestNMotifs(var_profile, var_index, *m, *n, motifs, motifIndices, subsequenceIndices,
+                                       self_join);
+    } catch (std::invalid_argument &ia) {
+        std::cerr << ia.what() << std::endl;
+        exit(-1);
+    } catch (std::runtime_error &re) {
+        std::cerr << re.what() << std::endl;
+        exit(-1);
+    }
+
     af_retain_array(motif_distances, motifs.get());
     af_retain_array(motif_indices, motifIndices.get());
     af_retain_array(subsequence_indices, subsequenceIndices.get());
