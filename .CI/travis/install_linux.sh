4ba3e4390ff69112445392255ab17ff4987edb51
Oscar Torreno <oscar.torreno@shapelets.io>
2018-07-23 08:35:40 +0200

    Install rpm for rpm packaging. Add artifacts section to AppVeyor

--- .CI/travis/install_linux.sh
+++ .CI/travis/install_linux.sh
@@ -6,7 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 sudo apt-get update -y
-sudo apt-get install -y lcov python3 python3-pip
+sudo apt-get install -y lcov python3 python3-pip rpm
 
 # Installing conan
 sudo pip3 install conan
