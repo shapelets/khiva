25b1adbce2547e050f20bac3c1d59f7198f21e6c
Antonio Vilches <antonio.vilches@shapelets.io>
2018-12-14 11:12:38 +0100

    Adding PAA benchmark

---
+++ benchmarks/dimensionalityBench.cpp
@@ -0,0 +1,87 @@
+// Copyright (c) 2018 Shapelets.io
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <benchmark/benchmark.h>
+#include <khiva/dimensionality.h>
+#include "khivaBenchmark.h"
+
+template <af::Backend BE, int D>
+void PAADivisible(benchmark::State &state) {
+    af::setBackend(BE);
+    af::setDevice(D);
+
+    auto n = state.range(0);
+    auto m = state.range(1);
+    int bins = state.range(2);
+
+    auto t = af::randu(n, m, f32);
+
+    af::sync();
+    while (state.KeepRunning()) {
+        auto paa = khiva::dimensionality::PAA(t, bins);
+        paa.eval();
+        af::sync();
+    }
+    addMemoryCounters(state);
+}
+
+template <af::Backend BE, int D>
+void PAANonDivisible(benchmark::State &state) {
+    af::setBackend(BE);
+    af::setDevice(D);
+
+    auto n = state.range(0);
+    auto m = state.range(1);
+    int bins = state.range(2);
+
+    auto t = af::randu(n, m, f32);
+
+    af::sync();
+    while (state.KeepRunning()) {
+        auto paa = khiva::dimensionality::PAA(t, bins);
+        paa.eval();
+        af::sync();
+    }
+    addMemoryCounters(state);
+}
+
+void cudaBenchmarks() {
+    BENCHMARK_TEMPLATE(PAADivisible, af::Backend::AF_BACKEND_CUDA, CUDA_BENCHMARKING_DEVICE)
+        ->RangeMultiplier(2)
+        ->Ranges({{1 << 10, 256 << 10}, {2, 16}, {512, 512}})
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
+
+    BENCHMARK_TEMPLATE(PAANonDivisible, af::Backend::AF_BACKEND_CUDA, CUDA_BENCHMARKING_DEVICE)
+        ->RangeMultiplier(2)
+        ->Ranges({{1 << 10, 256 << 10}, {2, 16}, {300, 300}})
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
+}
+
+void openclBenchmarks() {
+    BENCHMARK_TEMPLATE(PAADivisible, af::Backend::AF_BACKEND_OPENCL, OPENCL_BENCHMARKING_DEVICE)
+        ->RangeMultiplier(2)
+        ->Ranges({{1 << 10, 256 << 10}, {2, 16}, {512, 512}})
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
+
+    BENCHMARK_TEMPLATE(PAANonDivisible, af::Backend::AF_BACKEND_OPENCL, OPENCL_BENCHMARKING_DEVICE)
+        ->RangeMultiplier(2)
+        ->Ranges({{1 << 10, 256 << 10}, {2, 16}, {300, 300}})
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
+}
+
+void cpuBenchmarks() {
+    BENCHMARK_TEMPLATE(PAADivisible, af::Backend::AF_BACKEND_CPU, CPU_BENCHMARKING_DEVICE)
+        ->RangeMultiplier(2)
+        ->Ranges({{1 << 10, 256 << 10}, {2, 16}, {512, 512}})
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
+
+    BENCHMARK_TEMPLATE(PAANonDivisible, af::Backend::AF_BACKEND_CPU, CPU_BENCHMARKING_DEVICE)
+        ->RangeMultiplier(2)
+        ->Ranges({{1 << 10, 256 << 10}, {2, 16}, {300, 300}})
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
+}
+
+KHIVA_BENCHMARK_MAIN(cudaBenchmarks, openclBenchmarks, cpuBenchmarks)
