4b08dd04f109cd271a419acd82d1fee8811a00c6
Antonio Vilches <antonio.vilches@shapelets.io>
2018-12-18 19:10:39 +0100

    Remove unused include header file.

--- src/khiva/distances.cpp
+++ src/khiva/distances.cpp
@@ -5,7 +5,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <khiva/distances.h>
-#include <khiva/features.h>
 #include <khiva/normalization.h>
 #include <algorithm>
 #include <limits>
