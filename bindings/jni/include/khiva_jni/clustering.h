2f3d571b7279f9687f7ec404ef3fd82e716ad7e6
Antonio Vilches <antonio.vilches@shapelets.io>
2019-02-19 09:10:25 +0100

    Adding forgotten files.

--- bindings/jni/include/khiva_jni/clustering.h
+++ bindings/jni/include/khiva_jni/clustering.h
@@ -13,33 +13,39 @@ extern "C" {
 /**
  * @brief Calculates the kMeans algorithm.
  *
- * @param tss            Expects an input array whose dimension zero is the length of the time series (all the same) and
- *                       dimension one indicates the number of time series.
- * @brief Calculates     The clusterization based on SBD.
- * @param k              The number of means to be computed.
- * @param ref_centroids  The resulting means or centroids.
- * @param ref_labels     The resulting labels of each time series which is the closest centroid.
- * @param tolerance      The error tolerance to stop the computation of the centroids.
- * @param maxIterations  The maximum number of iterations allowed.
+ * @param tss                   Expects an input array whose dimension zero is the length of the time series (all the
+ * same) and dimension one indicates the number of time series.
+ * @brief Calculates            The clusterization based on SBD.
+ * @param k                     The number of means to be computed.
+ * @param ref_initial_centroids The initial centroids.
+ * @param ref_centroids         The resulting means or centroids.
+ * @param ref_initial_labels    The initial labels.
+ * @param ref_labels            The resulting labels of each time series which is the closest centroid.
+ * @param tolerance             The error tolerance to stop the computation of the centroids.
+ * @param maxIterations         The maximum number of iterations allowed.
  */
 JNIEXPORT jlongArray JNICALL Java_io_shapelets_khiva_Clustering_kMeans(JNIEnv *env, jobject, jlong ref_tss, jint k,
-                                                                       jlong ref_centroids, jlong ref_labels,
+                                                                       jlong ref_initial_centroids, jlong ref_centroids,
+                                                                       jlong ref_initial_labels, jlong ref_labels,
                                                                        jfloat tolerance, jint maxIterations);
 
 /**
  * @brief Computes the kShape algorithm.
  *
- * @param tss            Expects an input array whose dimension zero is the length of the time series (all the same) and
- *                       dimension one indicates the number of time series.
- * @brief Calculates     The clusterization based on SBD.
- * @param k              The number of means to be computed.
- * @param ref_centroids  The resulting means or centroids.
- * @param ref_labels     The resulting labels of each time series which is the closest centroid.
- * @param tolerance      The error tolerance to stop the computation of the centroids.
- * @param maxIterations  The maximum number of iterations allowed.
+ * @param tss                   Expects an input array whose dimension zero is the length of the time series (all the
+ * same) and dimension one indicates the number of time series.
+ * @brief Calculates            The clusterization based on SBD.
+ * @param k                     The number of means to be computed.
+ * @param ref_initial_centroids The initial centroids.
+ * @param ref_centroids         The resulting means or centroids.
+ * @param ref_initial_labels    The initial labels.
+ * @param ref_labels            The resulting labels of each time series which is the closest centroid.
+ * @param tolerance             The error tolerance to stop the computation of the centroids.
+ * @param maxIterations         The maximum number of iterations allowed.
  */
 JNIEXPORT jlongArray JNICALL Java_io_shapelets_khiva_Clustering_kShape(JNIEnv *env, jobject, jlong ref_tss, jint k,
-                                                                       jlong ref_centroids, jlong ref_labels,
+                                                                       jlong ref_initial_centroids, jlong ref_centroids,
+                                                                       jlong ref_initial_labels, jlong ref_labels,
                                                                        jfloat tolerance, jint maxIterations);
 
 #ifdef __cplusplus
