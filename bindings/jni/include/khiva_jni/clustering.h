deca820f4f95a5361f4811b6a49b66e5731c7e32
Antonio Vilches <avilchesreina@gmail.com>
2019-03-07 15:14:07 +0100

    Fix/jniclustering2 (#90)
    * Fix style

--- bindings/jni/include/khiva_jni/clustering.h
+++ bindings/jni/include/khiva_jni/clustering.h
@@ -20,13 +20,12 @@ extern "C" {
  * same) and dimension one indicates the number of time series.
  * @brief Calculates            The clusterization based on SBD.
  * @param k                     The number of means to be computed.
- * @param ref_centroids         The resulting means or centroids.
- * @param ref_labels            The resulting labels of each time series which is the closest centroid.
  * @param tolerance             The error tolerance to stop the computation of the centroids.
  * @param maxIterations         The maximum number of iterations allowed.
+ *
+ * @return An Array of arrays with the resulting centroids and labels.
  */
 JNIEXPORT jlongArray JNICALL Java_io_shapelets_khiva_Clustering_kMeans(JNIEnv *env, jobject, jlong ref_tss, jint k,
-                                                                       jlong ref_centroids, jlong ref_labels,
                                                                        jfloat tolerance, jint maxIterations);
 
 /**
@@ -39,13 +38,10 @@ JNIEXPORT jlongArray JNICALL Java_io_shapelets_khiva_Clustering_kMeans(JNIEnv *e
  * same) and dimension one indicates the number of time series.
  * @brief Calculates            The clusterization based on SBD.
  * @param k                     The number of means to be computed.
- * @param ref_centroids         The resulting means or centroids.
- * @param ref_labels            The resulting labels of each time series which is the closest centroid.
  * @param tolerance             The error tolerance to stop the computation of the centroids.
  * @param maxIterations         The maximum number of iterations allowed.
  */
 JNIEXPORT jlongArray JNICALL Java_io_shapelets_khiva_Clustering_kShape(JNIEnv *env, jobject, jlong ref_tss, jint k,
-                                                                       jlong ref_centroids, jlong ref_labels,
                                                                        jfloat tolerance, jint maxIterations);
 
 #ifdef __cplusplus
