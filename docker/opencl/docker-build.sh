2e400bd8fc970801a787a2166e407acd85c45b77
Raúl Bocanegra <raul.bocanegra@shapelets.io>
2020-05-05 00:13:02 +0200

    Add KHIVA_BRANCH build-arg

--- docker/opencl/docker-build.sh
+++ docker/opencl/docker-build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 docker build --target khiva-opencl-builder-base -t $PUSH_REGISTRY/shapelets/khiva-opencl-builder-base:0.5.0 .
 docker build --target arrayfire-opencl-builder -t $PUSH_REGISTRY/shapelets/arrayfire-opencl-builder:3.6.2 .
-docker build --target khiva-builder -t $PUSH_REGISTRY/shapelets/khiva-builder:0.5.0 .
+docker build --target khiva-builder --build-arg KHIVA_BRANCH=v0.5.0 -t $PUSH_REGISTRY/shapelets/khiva-builder:0.5.0 .
 docker build --target arrayfire-intel-opencl -t $PUSH_REGISTRY/shapelets/arrayfire-intel-opencl:3.6.2 .
 docker build --target khiva-intel-opencl -t $PUSH_REGISTRY/shapelets/khiva:0.5.0-opencl .
 docker push $PUSH_REGISTRY/shapelets/khiva-opencl-builder-base:0.5.0
